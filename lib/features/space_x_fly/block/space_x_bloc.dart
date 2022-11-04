a46bcf63cdc00d91aa583cbe3925404220f89461
Ali <alisekerr2018@gmail.com>
2022-11-04 16:40:09 +0300

    Eklemeler

--- lib/features/space_x_fly/block/space_x_bloc.dart
+++ lib/features/space_x_fly/block/space_x_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:spacex/features/space_x_fly/block/space_x_event.dart';
 import 'package:spacex/features/space_x_fly/block/space_x_state.dart';
 import 'package:spacex/features/space_x_fly/repository/space_x_repository.dart';
@@ -16,5 +17,29 @@ class SpaceXBloc extends Bloc<SpaceXEvent, SpaceXState> {
         emit(SpaceXErrorState(e.toString()));
       }
     });
+  }
+
+  @override
+  void onEvent(SpaceXEvent event) {
+    debugPrint('onEvent: $event');
+    super.onEvent(event);
+  }
+
+  @override
+  void onTransition(Transition<SpaceXEvent, SpaceXState> transition) {
+    super.onTransition(transition);
+    debugPrint(transition.toString());
+  }
+
+  @override
+  void onChange(Change<SpaceXState> change) {
+    super.onChange(change);
+    debugPrint(change.toString());
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    debugPrint('$error, $stackTrace');
+    super.onError(error, stackTrace);
   }
 }
